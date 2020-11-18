053e25d5b44f8507f6c55397458d33daa41c354f
Adrian <karatsumo@gmail.com>
2020-11-18 21:14:31 +0100

    Primer commit

---
+++ src/Controller/LibrosController.php
@@ -0,0 +1,205 @@
+<?php
+namespace App\Controller;
+use App\Repository\LibrosRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class LibrosController
+ * @package App\Controller
+ *
+ * @Route(path="/api/")
+ */
+class LibrosController
+{
+    private $LibrosRepository;
+
+    public function __construct(LibrosRepository $LibrosRepository)
+    {
+        $this->LibrosRepository = $LibrosRepository;
+    }
+
+    /**
+     * @Route("libros/importador", name="add_libros", methods={"POST"})
+     */
+    public function addLibros(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $libros = $data['books'];
+
+        foreach ($libros as $libro) {
+            
+            $isbn = $libro['isbn'];
+            $title = $libro['title'];
+            $subtitle = $libro['subtitle'];
+            $author = $libro['author'];
+            $published = new \DateTime('@'.strtotime($libro['published']));
+            $publisher = $libro['publisher'];
+            $pages = $libro['pages'];
+            $description = $libro['description'];
+            $category = $libro['category'];
+            $website = $libro['website'];
+
+            $this->LibrosRepository->saveLibro($isbn, $title, $subtitle, $author, $published, $publisher, $pages, $description, $category, $website );
+
+        }
+
+        return new JsonResponse(['status' => 'Libros anyadidos!'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("libros", name="add_libro", methods={"POST"})
+     */
+    public function add(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $isbn = $data['isbn'];
+        $title = $data['title'];
+        $subtitle = $data['subtitle'];
+        $author = $data['author'];
+        //$published = new \DateTime('@'.strtotime('now'));
+        $published = new \DateTime('@'.strtotime($data['published']));
+        $publisher = $data['publisher'];
+        $pages = $data['pages'];
+        $description = $data['description'];
+        $category = $data['category'];
+        $website = $data['website'];
+
+
+
+        $this->LibrosRepository->saveLibro($isbn, $title, $subtitle, $author, $published, $publisher, $pages, $description, $category, $website );
+
+        return new JsonResponse(['status' => 'Libro anyadido!'], Response::HTTP_CREATED);
+    }
+
+    /**
+     * @Route("libros/{isbn}", name="get_libro", methods={"GET"})
+     */
+    public function get($isbn): JsonResponse
+    {
+        $libro = $this->LibrosRepository->findOneBy(['isbn' => $isbn]);
+
+        $data = [
+            'isbn' => $libro->getIsbn(), 
+            'title' => $libro->getTitle(),
+            'subtitle' => $libro->getSubtitle(),
+            'author' => $libro->getAuthor(),
+            'published' => $libro->getPublished(),
+            'publisher' => $libro->getPublisher(),
+            'pages' => $libro->getPages(),
+            'description' => $libro->getDescription(),
+            'category' => $libro->getCategory()
+        ];
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+
+    /**
+     * @Route("libros/category/{category}", name="get_libro_by_category", methods={"GET"})
+     */
+    public function getCategory($category): JsonResponse
+    {
+        $libros = $this->LibrosRepository->findBy(['category' => $category]);
+
+        $data = [
+            
+        ];
+
+
+        foreach ($libros as $libro) {
+            
+            $libro_insertado = [
+                'isbn' => $libro->getIsbn(),
+                'title' => $libro->getTitle(),
+                'subtitle' => $libro->getSubtitle(),
+                'author' => $libro->getAuthor(),
+                'published' => $libro->getPublished(),
+                'publisher' => $libro->getPublisher(),
+                'pages' => $libro->getPages(),
+                'description' => $libro->getDescription(),
+                'category' => $libro->getCategory()
+            ];
+
+            array_push($data, $libro_insertado);
+        }
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("libros", name="get_all_libros", methods={"GET"})
+     */
+    public function getAll(): JsonResponse
+    {
+        $libros = $this->LibrosRepository->findAll();
+        $data = [];
+
+        foreach ($libros as $libro) {
+            $libro_insertado = [
+                'isbn' => $libro->getIsbn(),
+                'title' => $libro->getTitle(),
+                'subtitle' => $libro->getSubtitle(),
+                'author' => $libro->getAuthor(),
+                'published' => $libro->getPublished(),
+                'publisher' => $libro->getPublisher(),
+                'pages' => $libro->getPages(),
+                'description' => $libro->getDescription(),
+                'category' => $libro->getCategory()
+            ];
+
+            array_push($data, $libro_insertado);
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("libros/{isbn}", name="delete_libros", methods={"DELETE"})
+     */
+    public function delete($isbn): JsonResponse
+    {
+        $libro = $this->LibrosRepository->findOneBy(['isbn' => $isbn]);
+
+        $this->LibrosRepository->removeLibro($libro);
+        
+        return new JsonResponse(['status' => 'Libro borrado!'], Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("libros/before/{date}", name="get_before_Date", methods={"GET"})
+     */
+    public function getBeforeDate($date): JsonResponse
+    {
+        $libros = $this->LibrosRepository->findAll();
+        $data = [];
+        $date_string = $date.'-01-01';
+        $date_before = new \DateTime('@'.strtotime($date_string));
+
+        foreach ($libros as $libro) {
+
+
+            $libro_insertado = [
+                'isbn' => $libro->getIsbn(),
+                'title' => $libro->getTitle(),
+                'subtitle' => $libro->getSubtitle(),
+                'author' => $libro->getAuthor(),
+                'published' => $libro->getPublished(),
+                'publisher' => $libro->getPublisher(),
+                'pages' => $libro->getPages(),
+                'description' => $libro->getDescription(),
+                'category' => $libro->getCategory()
+            ];
+
+            if($libro_insertado['published'] < $date_before)
+                array_push($data, $libro_insertado);
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+}
+
+?>
+
---
+++ src/Entity/Libros.php
@@ -0,0 +1,193 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LibrosRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=LibrosRepository::class)
+ */
+class Libros
+{
+     /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $isbn;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $subtitle;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $published;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $publisher;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $pages;
+
+    /**
+     * @ORM\Column(type="text", length=65535)
+     */
+    private $description;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $website;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $category;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setisbn(string $isbn): self
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSubtitle(): ?string
+    {
+        return $this->subtitle;
+    }
+
+    public function setSubtitle(?string $subtitle): self
+    {
+        $this->subtitle = $subtitle;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPublished(): ?\DateTimeInterface
+    {
+        return $this->published;
+    }
+
+    public function setPublished(\DateTimeInterface $published): self
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getPublisher(): ?string
+    {
+        return $this->publisher;
+    }
+
+    public function setPublisher(string $publisher): self
+    {
+        $this->publisher = $publisher;
+
+        return $this;
+    }
+
+    public function getPages(): ?int
+    {
+        return $this->pages;
+    }
+
+    public function setPages(int $pages): self
+    {
+        $this->pages = $pages;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getWebsite(): ?string
+    {
+        return $website->website;
+    }
+
+    public function setWebsite(?string $website): self
+    {
+        $this->website = $website;
+
+        return $this;
+    }
+
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+}
